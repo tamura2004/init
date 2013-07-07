7a3dff551333231a5a8bc86061ed3ca5306bc5f4
tamura2004 <tamura2004@gmail.com>
2013-07-07 12:49:42 +0900

    Change install chef and knife only when not installed

--- start_ruby2.0_chef.sh
+++ start_ruby2.0_chef.sh
@@ -3,6 +3,8 @@
 var=2.0
 ruby=ruby-2.0.0-p247
  
+cd
+
 # Install Libraries for build
 apt-get install -y zlib1g-dev libssl-dev libreadline6-dev libyaml-dev libxml2-dev libxslt-dev make rake git curl
 
@@ -25,26 +27,21 @@ if [ ! -e /usr/local/bin/ruby ]; then
   cd
 fi
 
-# Install Sublime Text3
-# wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3047_amd64.deb
-# dpkg -i sublime-text_build-3047_amd64.deb
-
 # Install Chef
-gem install chef --no-ri --no-rdoc
+result=$(knife -v | grep -c Chef)
+if [ $result -eq 0 ]; then
+  gem install chef --no-ri --no-rdoc
+else
+  echo "chef already installed"
+fi
 
 # Install Knife
-cd
-git clone git://github.com/matschaffer/knife-solo.git
-cd knife-solo
-rake install
-
-# # Inisialize chef repository
-# cd
-# knife solo init chef-repo
-# cd chef-repo
-# git init
-# git add .
-# git commit -m 'Initialize chef repository'
-
-
-
+if [ ! -d ~/knife-solo ]; then
+  cd
+  git clone git://github.com/matschaffer/knife-solo.git
+  cd knife-solo
+  rake install
+  sed -i "$a\knife[:solo_path] = '/tmp/chef-solo'" ~/.chef/knife.rb
+else
+  echo "knife already installed"
+fi
